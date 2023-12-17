1466b8b1e5574656233f214cb492e302f4728cbf
Mohd Ali Bin Naser <mohdalibin2001@gmail.com>
2023-12-17 17:37:37 -0330

    Added mongodb connection

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,5 @@
+// Port to listen for the app
 export const PORT = 5555;
+
+// Url to conect to our online MongoDB database server (NOTE: username:password => "root:root")
+export const mongoDBURL = "mongodb+srv://root:root@bookstore-mern.kkjhu7f.mongodb.net/?retryWrites=true&w=majority";
--- backend/index.js
+++ backend/index.js
@@ -4,10 +4,15 @@ import express from "express";
 // Importing the port to listen to defined in config.js
 import {PORT} from "./config.js";
 
-
-
 // Creating an instance of the Express application
 const app = express();
+
+// Creating a route for the homepage(Default route "/")
+app.get('/', (request, response) => {
+    console.log(request);
+    return response.status(234).send("Welcome to MERN Stack Tutorial");
+});
+
 
 // Function to listen to the port and return a callback function
 app.listen(PORT, () => {
