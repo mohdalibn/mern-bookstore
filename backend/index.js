04570c47474d5af39fdfb2bf17c0fa12cde6cc62
Mohd Ali Bin Naser <mohdalibin2001@gmail.com>
2023-12-17 21:57:23 -0330

    All routes completed including refatoring and CORS POLICY

--- backend/index.js
+++ backend/index.js
@@ -3,19 +3,42 @@
 import express from "express";
 // Importing the port to listen to defined in config.js
 import {PORT, mongoDBURL} from "./config.js";
-
 // Library to use mongoDB in our project
 import mongoose from 'mongoose';
+// Importing Book
+import {Book} from "./models/bookModel.js";
+// Importing Refactored router
+import booksRoute from "./routes/booksRoute.js"
+// Importing CORS to deal with CORS POLICY
+import cors from 'cors';
 
 // Creating an instance of the Express application
 const app = express();
 
-// Creating a route for the homepage(Default route "/")
+// Parsing the requests
+app.use(express.json());
+
+// Middleware for handling CORS POLICY
+// Option 1: Allow All Origins with Default of cors(*)
+/* `app.use(cors());` is a middleware function that allows Cross-Origin Resource Sharing (CORS) in
+the Express application. */
+app.use(cors());
+
+// Option 2: Allow Custom Origins
+app.use(cors({
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeader: [Content-type]
+}))
+
+// A route for the homepage(Default route "/")
 app.get('/', (request, response) => {
     console.log(request);
     return response.status(234).send("Welcome to MERN Stack Tutorial");
 });
 
+
+app.use('/books', booksRoute);
 
 
 // Connect to the mongoDB database with error handling
